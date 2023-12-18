08946a1dbc618ccfb819af7f47339f6f4d0012f6
lizewei <li@tiandy.com>
2023-12-18 11:49:47 +0800

    feat(db page): add a func that supply the db with page

--- datasource/database.go
+++ datasource/database.go
@@ -1,8 +1,13 @@
 package datasource
 
 import (
+	"github.com/healer1219/martini/global"
 	"gorm.io/gorm"
 )
+
+func PageDB(page int, pageSize int) *gorm.DB {
+	return Page(page, pageSize, global.DB())
+}
 
 func Page(page int, pageSize int, db *gorm.DB) *gorm.DB {
 	offset := (page - 1) * pageSize
