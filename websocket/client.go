f7c148c93700a48ffac7f71d527b3ba70a4d2f7a
lizewei <li@tiandy.com>
2023-12-21 10:54:57 +0800

    feat(web socket): websocket pool key holds generics

--- websocket/client.go
+++ websocket/client.go
@@ -7,32 +7,40 @@ import (
 )
 
 var (
-	clientPool = ConnPool{ConnMap: make(map[string]*websocket.Conn)}
+	clientPool = Default()
 )
 
-type ConnPool struct {
-	sync.RWMutex
-	ConnMap map[string]*websocket.Conn
+type PoolKey interface {
+	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | uintptr | float32 | float64 | string
 }
 
-func (c *ConnPool) New() *ConnPool {
-	return &ConnPool{
-		ConnMap: make(map[string]*websocket.Conn),
+type ConnPool[T PoolKey] struct {
+	sync.RWMutex
+	ConnMap map[T]*websocket.Conn
+}
+
+func Default() *ConnPool[string] {
+	return New[string]()
+}
+
+func New[T PoolKey]() *ConnPool[T] {
+	return &ConnPool[T]{
+		ConnMap: make(map[T]*websocket.Conn),
 	}
 }
 
-func (c *ConnPool) AddClient(id string, conn *websocket.Conn) error {
+func (c *ConnPool[T]) AddClient(id T, conn *websocket.Conn) error {
 	defer c.Unlock()
 	c.Lock()
 	_, ok := c.ConnMap[id]
 	if ok {
-		return errors.New("id exist")
+		return errors.New("conn id exist")
 	}
 	c.ConnMap[id] = conn
 	return nil
 }
 
-func (c *ConnPool) ForceAddClient(id string, conn *websocket.Conn) {
+func (c *ConnPool[T]) ForceAddClient(id T, conn *websocket.Conn) {
 	defer c.Unlock()
 	c.Lock()
 	c.ConnMap[id] = conn
